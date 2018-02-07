ca0af578a4c52274ed21429cf3a0cbf4799473ad
Emilie Dam Larsen <emiliedlarsen@gmail.com>
2018-02-07 13:36:05 +0100

    Ændringer

--- js/myScript.js
+++ js/myScript.js
@@ -4,11 +4,24 @@
 
 // variabler
 var tal = 1;
+
+//booleans
 var erDetRigtigt = false;
-var tekst = "Her er min tekst";
+var erDetRigtigt = true;
+
+//en streng (noget tekst)
+var ligeHvadViVil = "<h1>Julemanden findes og jorden er flad.</h1>";
+
+//klister variable sammen
+var and = "Mit navn er Anders. Anders And."
+
+//datatyper
+
 
 // kommentarer
 // regning 
+var ind = 2345;
+var ud = 9999;
 // datoer
 // klistre strenge sammen
 // lister (scratch: data)
@@ -18,6 +31,10 @@ var tekst = "Her er min tekst";
 // objekter
 
 /* se resultatet af din kode i browserens udviklings-værktøj */
-console.log( "Tallet er lige nu: " + tal );
+console.log(ligeHvadViVil + " " + and);
+console.log((ind - ud)*12);
+
+//Skriv i browservinduet
+document.write(ligeHvadViVil);
 
 // næste gang: events.
